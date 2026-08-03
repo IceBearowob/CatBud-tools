d24376b054ae6f2a2202e5fc8ed1010c7c71a054
IceBear <IceBearowob@users.noreply.github.com>
2026-08-03 15:25:06 +0800

    將fabric example弄上去+嘗試按鍵觸發功能

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CatBud tools
 
+目前來說(((   
+拿來練手用的，第一次寫模組
+
+## Setup
+
+For setup instructions, please see the [Fabric Documentation page](https://docs.fabricmc.net/develop/getting-started/creating-a-project#setting-up) related to the IDE that you are using.
+
+## License
+
+This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
--- src/client/java/ice/catbudtools/client/CatBudToolsClient.java
+++ src/client/java/ice/catbudtools/client/CatBudToolsClient.java
@@ -1,10 +1,36 @@
 package ice.catbudtools.client;
 
+import ice.catbudtools.CatBudTools;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class CatBudToolsClient implements ClientModInitializer {
+	private static final KeyBinding.Category ITEM_QUERY_CATEGORY = KeyBinding.Category.create(
+			CatBudTools.id("item_query")
+	);
+
+	private static final KeyBinding OPEN_ITEM_QUERY_KEY = KeyBindingHelper.registerKeyBinding(
+			new KeyBinding(
+					"key.catbud-tools.open_item_query",
+					InputUtil.Type.KEYSYM,
+					GLFW.GLFW_KEY_G,
+					ITEM_QUERY_CATEGORY
+			)
+	);
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (OPEN_ITEM_QUERY_KEY.wasPressed()) {
+				if (client.player != null) {
+					client.player.sendMessage(Text.literal("貓芽特殊物品查詢：功能準備中"), false);
+				}
+			}
+		});
 	}
 }
